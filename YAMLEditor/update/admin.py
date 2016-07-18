cf1956eece44c26f5364a002a08fb1ccfa4866dc
Matthew Rubenstein <mrubes375@gmail.com>
2016-07-17 22:00:25 -0400

    Added navbar and models

--- YAMLEditor/update/admin.py
+++ YAMLEditor/update/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Change
 # Register your models here.
+
+@admin.register(Change)
+class ChangesRegister(admin.ModelAdmin):
+    pass
--- YAMLEditor/update/models.py
+++ YAMLEditor/update/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Change(models.Model):
+    date = models.DateTimeField()
+    files_changed = models.TextField()
+    user = models.CharField(max_length=100)
+    template = models.CharField(max_length=50)
+    old_context = models.CharField(max_length=1000)
+    new_context = models.CharField(max_length=1000)
