e36164bb169e8ba1d54c7155d7fdc2060033617e
Matthew Rubenstein <mrubes375@gmail.com>
2016-07-22 17:15:50 -0400

    Added button to navbar to toggle edit mode

--- YAMLEditor/static/js/editor.js
+++ YAMLEditor/static/js/editor.js
@@ -1,6 +1,21 @@
 var containsUpdate = window.location.pathname.indexOf('update')!=-1;
 // var top_path = path.substring(1, 7);
-if (!containsUpdate) {
-    $("body *").mousedown(function() {
-        $(this).attr('contentEditable', 'true');
-})};
+if (containsUpdate) {
+    $(".edit").toggle(function() {
+        $("body *").attr('contentEditable', 'true');
+        $(this).removeClass('btn-danger');
+        $(this).addClass('btn-success');
+        console.log(this);
+    }, function() {
+        $("body *").removeAttr('contentEditable');
+        $(this).removeClass('btn-success');
+        $(this).addClass('btn-danger');
+        console.log(this);
+    });
+}
+
+// console.log($("body *").attr('contentEditable'));
+// if (!containsUpdate) {
+//     $("body *").mousedown(function() {
+//         $(this).attr('contentEditable', 'true');
+// })};
